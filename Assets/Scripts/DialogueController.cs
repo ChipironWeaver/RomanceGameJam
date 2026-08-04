547c51bbc054b59004f8ac6f551de746ad5c84f9
Lowen <lowen.tisserand@gmail.com>
2026-08-04 20:08:35 +0200

    start with no animations

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -1,32 +1,68 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
 
 public class DialogueController : MonoBehaviour
 {
     [SerializeField] private float _dialogueSkipCooldown;
     
+    [Header("References")]
+    [SerializeField] private GameObject _dialoguePanel;
+    [SerializeField] private TextMeshProUGUI _textMeshProUGUI;
+    [Header("Tests")]
+    [SerializeField] private DialogueSequence _testDialogueSequence;
+    
     private float _timeSinceLastDialogue;
+    private DialogueSequence _currentDialogueSequence;
+    private int _dialogueSequenceIndex;
+    private Sequence _sequence;
 
-    public void TryDialogue()
+    public void TryDialogue(bool force = false)
     {
-        if (Time.time > _timeSinceLastDialogue + _dialogueSkipCooldown)
+        if (!(Time.time > _timeSinceLastDialogue + _dialogueSkipCooldown) && !force) return;
+        
+        if (_dialogueSequenceIndex >= _currentDialogueSequence.dialogues.Count)
         {
-            print("Yes");
+            EndOfSequence();
+            return;
         }
-        else print("nuh uh");
+        
+        print("Displaying Dialogue " + _dialogueSequenceIndex + " in " + _currentDialogueSequence.name);
+        DisplayDialogue(_currentDialogueSequence.dialogues[_dialogueSequenceIndex]);
+        
+        _dialogueSequenceIndex++;
     }
     
     public void StartDialogue(DialogueSequence dialogueSequence)
     {
-        
+        SetActivation(true);
+        _currentDialogueSequence = dialogueSequence;
+        _dialogueSequenceIndex = 0;
+        TryDialogue(true);
     }
 
-    public float EstimateDialogueSequenceTime(DialogueSequence dialogueSequence)
+    public void SetActivation(bool active)
     {
-        return 67f;
+        _dialoguePanel.SetActive(active);
     }
 
-    public float EstimateDialogueTime(Dialogue dialogue)
+    public void DisplayDialogue(Dialogue dialogue)
     {
-        return 6.7f;
+        _textMeshProUGUI.text = dialogue.dialogueText;
+        _textMeshProUGUI.maxVisibleCharacters = 67;
+    }
+
+    public void EndOfSequence()
+    {
+        print("this is the end of the sequence");
+        SetActivation(false);
+    }
+
+    
+    [Button]
+    public void TestDialogueSequence()
+    {
+        StartDialogue(_testDialogueSequence);
     }
 }
