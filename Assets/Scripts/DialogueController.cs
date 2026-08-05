6169a8f0b3f2f4ebe4e6906ec0eafa72374cfba4
Lowen <lowen.tisserand@gmail.com>
2026-08-05 18:55:32 +0200

    Type writting effect

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using NaughtyAttributes;
@@ -9,6 +11,13 @@ public class DialogueController : MonoBehaviour
 {
     [SerializeField] private float _dialogueSkipCooldown;
     [SerializeField] private GameObject _choiceButtonPrefab;
+    [Header("TypeWriterSettings")]
+    [SerializeField] private float _speedMultiplier;
+    [SerializeField] private float _skippingSpeedMultiplier;
+    [SerializeField] private float _timeToWaitPerCharacter;
+    [SerializeField] private float _timeToWaitForSpaces;
+    [SerializeField] private List<string> _hiddenCharacters;
+    [SerializeField] private List<TypeWriterCharacterWait> _specialCharacterWaits = new List<TypeWriterCharacterWait>();
     [Header("References")]
     [SerializeField] private GameObject _dialoguePanel;
     [SerializeField] private GameObject _choicePanel;
@@ -20,8 +29,42 @@ public class DialogueController : MonoBehaviour
     private DialogueSequence _currentDialogueSequence;
     private int _dialogueSequenceIndex;
     private Sequence _sequence;
+    
+    private int _typeWriterState; //0 = not writing 1 = writing 2 = writing fast 3 = Force End It
+    private int _typeWriterIndex;
+    private float _typeWriterTimer;
+    List<float> _typeWriterWaitTimes = new List<float>();
+    
+    
     private List<ChoiceButton> _choicesButtons = new List<ChoiceButton>();
+    private Dictionary<char, float> _specialCharacterWaitsDictionary;
 
+    public void Update()
+    {
+        if (_typeWriterState > 0)
+        {
+            if (_typeWriterState == 3)
+            {
+                _textMeshProUGUI.maxVisibleCharacters = _typeWriterWaitTimes.Count;
+                _typeWriterState = 0;
+                return;
+            }
+            _typeWriterTimer += Time.deltaTime;
+            if (_typeWriterTimer >= _typeWriterWaitTimes[_typeWriterIndex] /
+                     (_speedMultiplier * (_typeWriterState > 1 ? _skippingSpeedMultiplier : 1)))
+            {
+                _typeWriterIndex++;
+                _typeWriterTimer = 0;
+                _textMeshProUGUI.maxVisibleCharacters++;
+                if (_typeWriterIndex >= _typeWriterWaitTimes.Count)
+                {
+                    _textMeshProUGUI.maxVisibleCharacters = _typeWriterWaitTimes.Count;
+                    _typeWriterState = 0;
+                }
+            }
+        }
+    }
+    
     public void TryDialogue(bool force = false)
     {
         if (!(Time.time > _timeSinceLastDialogue + _dialogueSkipCooldown) && !force)
@@ -50,17 +93,94 @@ public class DialogueController : MonoBehaviour
         TryDialogue(true);
     }
 
-    public void SetActivation(bool active)
+    public void DisplayDialogue(Dialogue dialogue, bool skipAnimation = false)
     {
-        _dialoguePanel.SetActive(active);
+        if (_specialCharacterWaitsDictionary == null)
+        {
+            _specialCharacterWaitsDictionary = new Dictionary<char, float>();
+            foreach (TypeWriterCharacterWait wait in _specialCharacterWaits)
+            {
+                _specialCharacterWaitsDictionary.Add(wait.character.ToCharArray()[0], wait.waitTime);
+            }
+        }
+        
+        string textFinalString = "";
+        _typeWriterWaitTimes = new List<float>();
+        bool isInMarkdown = false;
+        bool previousCharacterHiddenSpecialTime = false;
+        bool ignoreSpecialBehavior = false;
+        
+        foreach(char character in dialogue.dialogueText)
+        {
+            if (ignoreSpecialBehavior)
+            {
+                textFinalString += character;
+                _typeWriterWaitTimes.Add(_timeToWaitPerCharacter);
+                ignoreSpecialBehavior = false;
+            }
+            else if (isInMarkdown)
+            {
+                textFinalString += character;
+                if (character == ">".ToCharArray()[0])
+                {
+                    isInMarkdown = false;
+                }
+            }
+            else if (character == "$".ToCharArray()[0])
+            {
+                ignoreSpecialBehavior = true;
+            }
+            else if (character == "<".ToCharArray()[0])
+            {
+                textFinalString += character;
+                isInMarkdown = true;
+            }
+            else if (character == " ".ToCharArray()[0])
+            {
+                textFinalString += character;
+                _typeWriterWaitTimes.Add(_timeToWaitForSpaces);
+            }
+            else if (_specialCharacterWaitsDictionary.ContainsKey(character) && !previousCharacterHiddenSpecialTime)
+            {
+                _typeWriterWaitTimes.Add(_specialCharacterWaitsDictionary[character]);
+                if (!_hiddenCharacters.Contains(character.ToString()))
+                {
+                    textFinalString += character;
+                }
+                else
+                {
+                    previousCharacterHiddenSpecialTime = true;
+                }
+            }
+            else
+            {
+                textFinalString += character;
+                if(!previousCharacterHiddenSpecialTime)
+                {
+                    _typeWriterWaitTimes.Add(_timeToWaitPerCharacter);
+                }
+                else
+                {
+                    previousCharacterHiddenSpecialTime = false;
+                }
+            }
+        }
+        
+        _textMeshProUGUI.text = textFinalString;
+        if(skipAnimation)
+        {
+            _textMeshProUGUI.maxVisibleCharacters = _typeWriterWaitTimes.Count;
+        }
+        else
+        {
+            print("time slot :" + _typeWriterWaitTimes.Count);
+            _textMeshProUGUI.maxVisibleCharacters = 0;
+            _typeWriterState = 1;
+            _typeWriterIndex = 0;
+            _typeWriterTimer = 0f;
+        }
     }
-
-    public void DisplayDialogue(Dialogue dialogue)
-    {
-        _textMeshProUGUI.text = dialogue.dialogueText;
-        _textMeshProUGUI.maxVisibleCharacters = 67;
-    }
-
+    
     public void EndOfSequence()
     {
         print("End of Sequence");
@@ -100,4 +220,15 @@ public class DialogueController : MonoBehaviour
     {
         StartDialogue(_testDialogueSequence);
     }
+    public void SetActivation(bool active)
+    {
+        _dialoguePanel.SetActive(active);
+    }
+}
+
+[Serializable]
+public class TypeWriterCharacterWait
+{
+    public string character;
+    public float waitTime;
 }
