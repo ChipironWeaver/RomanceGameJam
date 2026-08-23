d2bc5a1b99b665e732335b1eef95de9d1c7fa23a
Lowen <lowen.tisserand@gmail.com>
2026-08-23 21:06:22 +0200

    Smoother Music switch + Icon

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,21 +13,64 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private List<AudioClip> _uiClickSfx = new List<AudioClip>();
     [SerializeField] private List<AudioClip> _chattingSfx = new List<AudioClip>();
     [SerializeField] private List<AudioClip> _music = new List<AudioClip>();
+
+    [SerializeField] private int _autoMusicPlay = -1;
+    private int _activeMusic = -1;
+    [SerializeField] private float _musicTransitionTime = 0.5f;
     
     [SerializeField] private AudioSource _sfxAudioSource;
     [SerializeField] private AudioSource _musicAudioSource;
     
     [SerializeField] private bool _playMusicOnAwake;
-    
+    [SerializeField] private bool _continuePlayingMusicOnStop;
+
+    private bool _startedPlaying;
+    private void Update()
+    {
+        if (!_musicAudioSource.isPlaying && !_startedPlaying && _continuePlayingMusicOnStop)
+        {
+            _startedPlaying = true;
+            int index = Random.Range(1, _music.Count);
+            if (index == _activeMusic) index++;
+            if (index >= _music.Count) index = 1;
+            PlayMusic(index);
+        }
+    }
+
     void OnEnable()
     {
         Singleton();
     }
+
+    void Start()
+    {
+        if (_autoMusicPlay >= 0 && _playMusicOnAwake && _autoMusicPlay < _music.Count)
+        {
+            PlayMusic(_autoMusicPlay);
+        }
+    }
     
     public void PlayMusic(int index)
     {
-        _musicAudioSource.clip = _music[index];
-        _musicAudioSource.Play();
+        if (index == _activeMusic) return;
+        _activeMusic = index;
+        if(_activeMusic == -1)
+        {
+            _musicAudioSource.clip = _music[index];
+            _musicAudioSource.Play();
+        }
+        else
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(_musicAudioSource.DOFade(0, _musicTransitionTime/2));
+            sequence.AppendCallback(() =>
+            {
+                _musicAudioSource.clip = _music[index];
+                _musicAudioSource.Play();
+            });
+            sequence.Append(_musicAudioSource.DOFade(1, _musicTransitionTime/2));
+            sequence.OnComplete((() => _startedPlaying = false));
+        }
     }
 
     public void PlaySfx(SfxType type)
